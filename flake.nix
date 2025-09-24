0f46791266e25720604b293378fc570427e5a835
Venca H <venca.hyncica@gmail.com>
2025-09-24 22:53:22 +0200

    [Feature] Added runnable web2py_server app.

--- flake.nix
+++ flake.nix
@@ -13,14 +13,18 @@
         python = pkgs.python3;
 
         pythonDeps = python.pkgs.buildPythonApplication {
-          pname = "sprint_planner";
+          pname = "web2py_server";
           version = "0.1.0";
           src = pkgs.lib.cleanSource ./.;
 
           format = "pyproject";
 
-          nativeBuildInputs = with python.pkgs; [ setuptools wheel ];
-          buildInputs = with python.pkgs; [
+          nativeBuildInputs = with python.pkgs; [ 
+            setuptools 
+            wheel
+            pytest
+          ];
+          propagatedBuildInputs = with python.pkgs; [
             python-lorem
             google-cloud-firestore
             twisted
@@ -30,6 +34,19 @@
       in {
         packages.default = pythonDeps;
         packages.web2py_server = pythonDeps;
-    });
+        apps.web2py_server = 
+        let
+          web2pyInterpreter = pkgs.python3.withPackages (
+            ps: self.packages.${system}.web2py_server.propagatedBuildInputs
+          );
+        in { 
+          type = "app";
+          program =  "${pkgs.writeShellScriptBin "web2py-server" ''
+            exec ${web2pyInterpreter}/bin/python ${pythonDeps}/${python.sitePackages}/anyserver.py \
+              -s twisted "$@"
+          ''}/bin/web2py-server";
+        };
+      }
+  );
 }
 
